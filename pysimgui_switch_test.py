d191bcce884caa4ab04eecf715b340cb1189afe6
Pyroseza <jarpri08@gmail.com>
2018-10-31 09:27:08 +0000

    added an example of how to change a form on the fly instead of switching forms

---
+++ pysimgui_switch_test.py
@@ -0,0 +1,39 @@
+import PySimpleGUI as sg
+import time
+
+using_imperial = True
+exiting = False
+
+imperial_choices = ['Inches','Yards','Miles','Feet','Pounds','Ounces','Gallon']
+metric_choices = ['Millimeters','Centimeters','Meters','Kilometers','Kilograms','Grams','Liters']
+
+imperial_layout = [[sg.Text('Enter a value:')],
+          [sg.InputText(key='value')], 
+          [sg.Text('Choose a metric unit to convert to:')],
+          [sg.InputCombo(imperial_choices, size=(20, 5), key='imperial_choice')],
+          [sg.RButton('Calculate'), sg.RButton('Switch'), sg.Exit()]]
+          
+metric_layout = [[sg.Text('Enter a value:')],
+          [sg.InputText(key='value')], 
+          [sg.Text('Choose an imperial unit to convert to:')],
+          [sg.InputCombo(imperial_choices, size=(20, 5), key='imperial_choice')],
+          [sg.RButton('Calculate'), sg.RButton('Switch'), sg.Exit()]]
+
+while exiting == False:
+    window = sg.Window('Imperial / Metric calculator').Layout(imperial_layout if using_imperial else metric_layout)
+    while True:
+        event, values = window.Read()
+        if event is None or event == 'Exit':
+            exiting = True
+            break
+        if event == 'Switch':
+            # swap the form by changing flag and breaking out of the loop
+            using_imperial =  not using_imperial
+            break
+        if event == 'Calculate':
+            sg.Popup('Calculate was pressed!', 'value to convert is: {}'.format(values['value']), 'imperial unit to convert is: {}'.format(values['imperial_choice']))
+        print(event, values)
+        time.sleep(0.01)
+    window.CloseNonBlocking()
+    window = None
+    time.sleep(0.01)
